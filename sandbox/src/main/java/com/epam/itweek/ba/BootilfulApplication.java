17f91acf3856887eeb86cd4a4433c456f3c187a8
Izzet Mustsfaiev <webdizz@gmail.com>
2014-07-15 19:08:18 +0300

    Add basic application code

---
+++ sandbox/src/main/java/com/epam/itweek/ba/BootilfulApplication.java
@@ -0,0 +1,16 @@
+package com.epam.itweek.ba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class BootilfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootilfulApplication.class, args);
+    }
+}
