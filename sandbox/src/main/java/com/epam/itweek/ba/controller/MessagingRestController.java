0d892b77e1abefd706ed919091c44aeb9da6012d
Izzet Mustsfaiev <webdizz@gmail.com>
2014-07-16 09:31:53 +0300

    Add messages creation

--- sandbox/src/main/java/com/epam/itweek/ba/controller/MessagingRestController.java
+++ sandbox/src/main/java/com/epam/itweek/ba/controller/MessagingRestController.java
@@ -5,8 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sun.javafx.tools.packager.Log;
 
 @RestController
 @Slf4j
@@ -17,5 +20,10 @@ public class MessagingRestController {
     public String messagesFor(@PathVariable("username") @NotEmpty String username) {
         log.info("Is about to list messages");
         return "Hello, " + username;
+    }
+
+    @RequestMapping(value = "/{username}", method = RequestMethod.POST)
+    public void newMessage(@PathVariable("username") String username, String message) {
+        log.info("Is about to create new message");
     }
 }
