e9110a3a3f13d141b99595d424f8ad8cb72a13ac
Izzet Mustsfaiev <webdizz@gmail.com>
2014-07-23 00:14:42 +0300

    Change Message signature

--- sandbox/src/main/java/com/epam/itweek/ba/domain/Message.java
+++ sandbox/src/main/java/com/epam/itweek/ba/domain/Message.java
@@ -1,14 +1,14 @@
 package com.epam.itweek.ba.domain;
 
-import lombok.Data;
-
 import java.io.Serializable;
 import java.time.ZonedDateTime;
+
+import lombok.Data;
 
 @Data
 public class Message implements Serializable {
 
     private String uid;
-    private String message;
+    private String content;
     private ZonedDateTime posted;
 }
